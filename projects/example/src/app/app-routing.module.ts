8a9221bfb85a2d16c0c491ca44102bb67973af94
ofirf <ofirf@sentinel.com>
2021-01-14 13:18:19 +0200

    first commit

--- projects/example/src/app/app-routing.module.ts
+++ projects/example/src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { InputModule } from 'sentinelone-components-lib';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), InputModule],
+  imports: [RouterModule.forRoot(routes), InputModule, BrowserAnimationsModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- projects/example/src/app/app.component.ts
+++ projects/example/src/app/app.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 const cells = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('myInsertRemoveTrigger', [
+      transition(':enter', [
+        style({ height: 0 }),
+        animate('200ms', style({ height: '*' })),
+      ]),
+      transition(':leave', [
+        animate('200ms', style({ height: 0 }))
+      ])
+    ]),
+  ]
 })
 export class AppComponent {
   header = cells;
