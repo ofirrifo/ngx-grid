74545671ffee987fd1a9052582dafbcd36f4fe4d
ofirf <ofirf@sentinel.com>
2021-01-23 17:37:48 +0200

    first commit

--- projects/example/src/app/app.component.ts
+++ projects/example/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { animate, style, transition, trigger } from '@angular/animations';
-import { iterator } from 'rxjs/internal-compatibility';
 
 const cells = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
 
